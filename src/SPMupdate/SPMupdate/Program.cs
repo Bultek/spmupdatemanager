c081363ed986f02a8e9048ae0fed2cecacc629ee
Demir Yerli <demiryerli@gmail.com>
2022-05-12 21:55:23 +0300

    "degitlabify"

--- src/SPMupdate/SPMupdate/Program.cs
+++ src/SPMupdate/SPMupdate/Program.cs
@@ -144,7 +144,7 @@ namespace SPMupdateManager
                 }
                 else
                 {
-                    return "https://gitlab.com/bultekdev/spm-projects/SharpPackageManager/-/raw/versioncontrol/" + Branch + ".spmvi";
+                    return "https://github.com/Bultek/SharpPackageManager/tree/versioncontrol/" + Branch + ".spmvi";
                 }
             } 
         }
